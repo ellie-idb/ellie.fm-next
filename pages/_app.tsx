3dc272b5459743a7620cf11c444d2b440a613935
hatf0 <harrison@0xcc.pw>
2023-03-01 15:07:36 -0500

    add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.scss';
 import Image from 'next/image';
 import type { AppProps } from 'next/app'
 import { ThemeProvider, GlobalStyle } from '@react95/core';
+import { computer3Data } from '@react95/icons';
 import { createGlobalStyle } from 'styled-components'
 import Head from 'next/head';
 
@@ -29,6 +30,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta content="object" property="og:type" />
         <meta content="https://ud2.sh" property="og:url" />
         <meta content="#37474f" data-react-helmet="true" name="theme-color" />
+        <link rel='icon' type='image/x-icon' href={computer3Data['32x32_4'].imageSrc} />
       </Head>
       <ThemeProvider>
         <GlobalStyle />
